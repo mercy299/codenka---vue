cfb064b92c0fa0ce869270a7512d61ef764133ac
mercy299 <mercysam299@gmail.com>
2023-11-05 21:30:29 +0100

    feat: modifications for wide screens

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,20 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Browse from '../views/Browse.vue'
-import Author from '../views/Author.vue'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
-import Forgotpasswd from '../views/Forgotpasswd.vue'
-import Resetpasswd from '../views/Resetpasswd.vue'
+import routes from './routes'
 
 
-const routes = [
-    {path: '/', name: Home, component: Home},
-    {path: '/About', name: About, component: About},
-    {path: '/Browse', name: Browse, component: Browse},
-    {path: '/Author', name: Author, component: Author},
-    {path: '/Signup', name: Signup, component: Signup},
-    {path: '/Login', name: Login, component: Login},
-    {path: '/Forgotpasswd', name: Forgotpasswd, component: Forgotpasswd},
-    {path: '/Resetpasswd', name: Resetpasswd, component: Resetpasswd}
+// const routes = [
+//     {path: '/', name: Home, component: Home},
+//     {path: '/About', name: About, component: About},
+//     {path: '/Browse', name: Browse, component: Browse},
+//     {path: '/Author', name: Author, component: Author},
+//     {path: '/Signup', name: Signup, component: Signup},
+//     {path: '/Login', name: Login, component: Login},
+//     {path: '/Forgotpasswd', name: Forgotpasswd, component: Forgotpasswd},
+//     {path: '/Resetpasswd', name: Resetpasswd, component: Resetpasswd}
 
 
 
-]
+// ]
 
 const router = createRouter ( {
     history: createWebHistory(process.env.BASE_URL),
